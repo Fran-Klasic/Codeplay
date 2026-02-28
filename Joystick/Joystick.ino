9b21f361b6d78056116736167c274af3cea11f89
HeroesOfBrine <fklasich@gmail.com>
2026-02-28 10:46:19 +0100

    Update

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -5,39 +5,35 @@
 
 void setup() {
   // put your setup code here, to run once:
-    pinMode(GPIO41,INPUT_PULLUP);
-    pinMode(GPIO39,INPUT_PULLUP);
-    pinMode(GPIO40,INPUT_PULLUP);
-    pinMode(GPIO14,INPUT_PULLUP);
-    Serial.Begin(115200);
+  pinMode(GPIO41, INPUT_PULLUP);
+  pinMode(GPIO39, INPUT_PULLUP);
+  pinMode(GPIO40, INPUT_PULLUP);
+  pinMode(GPIO14, INPUT_PULLUP);
+  Serial.Begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    int Button1Value=digitalRead(41);
-    int Button2Value=digitalRead(39);
-    int Button3Value=digitalRead(40);
-    int Button4Value=digitalRead(14);
+  int Button1Value = digitalRead(41);
+  int Button2Value = digitalRead(39);
+  int Button3Value = digitalRead(40);
+  int Button4Value = digitalRead(14);
 
-    Serial.print("\nButton 1: ");
-    Serial.print(Button1Value);
-    Serial.print("\nButton 2: ");
-    Serial.print(Button2Value);
-    Serial.print("\nButton 3: ");
-    Serial.print(Button3Value);
-    Serial.print("\nButton 4: ");
-    Serial.print(Button4Value);
+  Serial.print("\nButton 1: ");
+  Serial.print(Button1Value);
+  Serial.print("\nButton 2: ");
+  Serial.print(Button2Value);
+  Serial.print("\nButton 3: ");
+  Serial.print(Button3Value);
+  Serial.print("\nButton 4: ");
+  Serial.print(Button4Value);
 
-    if(Button1Value){
-
-    }
-    if(Button2Value){
-      
-    }
-    if(Button3Value){
-      
-    }
-    if(Button4Value){
-      
-    }
+  if (Button1Value) {
+  }
+  if (Button2Value) {
+  }
+  if (Button3Value) {
+  }
+  if (Button4Value) {
+  }
 }
