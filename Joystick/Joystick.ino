a26f359b7d4e91a50f78d0f38d0e79611cef2e9f
Toni31415 <td.postanski.sanducic@gmail.com>
2026-02-28 10:45:22 +0100

    update

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -9,6 +9,7 @@ void setup() {
     pinMode(GPIO39,INPUT_PULLUP);
     pinMode(GPIO40,INPUT_PULLUP);
     pinMode(GPIO14,INPUT_PULLUP);
+    Serial.Begin(115200);
 }
 
 void loop() {
@@ -17,6 +18,15 @@ void loop() {
     int Button2Value=digitalRead(39);
     int Button3Value=digitalRead(40);
     int Button4Value=digitalRead(14);
+
+    Serial.print("\nButton 1: ");
+    Serial.print(Button1Value);
+    Serial.print("\nButton 2: ");
+    Serial.print(Button2Value);
+    Serial.print("\nButton 3: ");
+    Serial.print(Button3Value);
+    Serial.print("\nButton 4: ");
+    Serial.print(Button4Value);
 
     if(Button1Value){
 
@@ -31,4 +41,3 @@ void loop() {
       
     }
 }
-
