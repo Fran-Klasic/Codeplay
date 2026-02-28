9821d039aa76d70590c87fe5b1252dacc8f8be3f
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2026-02-28 12:58:01 +0200

    Buttons_end

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,5 +1,4 @@
-#include <Arduino_LSM6DS3.h>
-#include <FastLED.h>
+
 
 #define GPIO41 41
 #define GPIO39 39
@@ -18,48 +17,34 @@ typedef struct {
 joystick_packet_t joystick = {0};
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(GPIO41, INPUT_PULLUP);
   pinMode(GPIO39, INPUT_PULLUP);
   pinMode(GPIO40, INPUT_PULLUP);
   pinMode(GPIO14, INPUT_PULLUP);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  int Button1Value = digitalRead(GPIO41);
-  int Button2Value = digitalRead(GPIO39);
-  int Button3Value = digitalRead(GPIO40);
-  int Button4Value = digitalRead(GPIO14);
-
-  Serial.print("\nButton 1: ");
-  Serial.print(!Button1Value);
-  Serial.print("\tButton 2: ");
-  Serial.print(!Button2Value);
-  Serial.print("\tButton 3: ");
-  Serial.print(!Button3Value);
-  Serial.print("\tButton 4: ");
-  Serial.print(!Button4Value);
-  
-
+  int Button1Value = !digitalRead(GPIO41);
+  int Button2Value = !digitalRead(GPIO39);
+  int Button3Value = !digitalRead(GPIO40);
+  int Button4Value = !digitalRead(GPIO14);
 
   int buttons = 0b00000;
-  if (!Button1Value) {
-    buttons += 0b00001;
-    joystick.buttons= buttons;
+  if (Button1Value) {
+    buttons |= 0b00001;
   }
-  if (!Button2Value) {
-    buttons += 0b00010;
-    joystick.buttons= buttons;
+  if (Button2Value) {
+    buttons |= 0b00100;
   }
-  if (!Button3Value) {
-    buttons += 0b00100;
-    joystick.buttons= buttons;
+  if (Button3Value) {
+    buttons |= 0b00010; 
   }
-  if (!Button4Value) {
-    buttons += 0b01000;
-    joystick.buttons= buttons;
+  if (Button4Value) {  
+    buttons |= 0b01000;
   }
-  Serial.write((uint8_t*)&joystick, sizeof(joystick));
 
+  joystick.buttons= buttons;
+  Serial.write((uint8_t*)&joystick, sizeof(joystick));
+  delay(10);
 }
